9e539cc1c49042fa223dd81220ac8cef33d36eda
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-03-01 22:13:20 -0300

    Adicionar arquivos de projeto.

---
+++ RadioButton/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace RadioButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pctrFramboesa = new System.Windows.Forms.PictureBox();
+            this.pctrMorango = new System.Windows.Forms.PictureBox();
+            this.pctrTomate = new System.Windows.Forms.PictureBox();
+            this.pctrMacaVerde = new System.Windows.Forms.PictureBox();
+            this.pctrKiwi = new System.Windows.Forms.PictureBox();
+            this.pctrLimao = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrFramboesa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrMorango)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrTomate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrMacaVerde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrKiwi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrLimao)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctrFramboesa
+            // 
+            this.pctrFramboesa.Image = ((System.Drawing.Image)(resources.GetObject("pctrFramboesa.Image")));
+            this.pctrFramboesa.Location = new System.Drawing.Point(85, 277);
+            this.pctrFramboesa.Name = "pctrFramboesa";
+            this.pctrFramboesa.Size = new System.Drawing.Size(178, 211);
+            this.pctrFramboesa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrFramboesa.TabIndex = 1;
+            this.pctrFramboesa.TabStop = false;
+            // 
+            // pctrMorango
+            // 
+            this.pctrMorango.Image = ((System.Drawing.Image)(resources.GetObject("pctrMorango.Image")));
+            this.pctrMorango.Location = new System.Drawing.Point(85, 60);
+            this.pctrMorango.Name = "pctrMorango";
+            this.pctrMorango.Size = new System.Drawing.Size(178, 211);
+            this.pctrMorango.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrMorango.TabIndex = 0;
+            this.pctrMorango.TabStop = false;
+            // 
+            // pctrTomate
+            // 
+            this.pctrTomate.Image = ((System.Drawing.Image)(resources.GetObject("pctrTomate.Image")));
+            this.pctrTomate.Location = new System.Drawing.Point(85, 494);
+            this.pctrTomate.Name = "pctrTomate";
+            this.pctrTomate.Size = new System.Drawing.Size(178, 211);
+            this.pctrTomate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrTomate.TabIndex = 2;
+            this.pctrTomate.TabStop = false;
+            // 
+            // pctrMacaVerde
+            // 
+            this.pctrMacaVerde.Image = ((System.Drawing.Image)(resources.GetObject("pctrMacaVerde.Image")));
+            this.pctrMacaVerde.Location = new System.Drawing.Point(497, 60);
+            this.pctrMacaVerde.Name = "pctrMacaVerde";
+            this.pctrMacaVerde.Size = new System.Drawing.Size(178, 211);
+            this.pctrMacaVerde.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrMacaVerde.TabIndex = 3;
+            this.pctrMacaVerde.TabStop = false;
+            // 
+            // pctrKiwi
+            // 
+            this.pctrKiwi.Image = ((System.Drawing.Image)(resources.GetObject("pctrKiwi.Image")));
+            this.pctrKiwi.Location = new System.Drawing.Point(497, 277);
+            this.pctrKiwi.Name = "pctrKiwi";
+            this.pctrKiwi.Size = new System.Drawing.Size(178, 211);
+            this.pctrKiwi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrKiwi.TabIndex = 4;
+            this.pctrKiwi.TabStop = false;
+            // 
+            // pctrLimao
+            // 
+            this.pctrLimao.Image = ((System.Drawing.Image)(resources.GetObject("pctrLimao.Image")));
+            this.pctrLimao.Location = new System.Drawing.Point(497, 494);
+            this.pctrLimao.Name = "pctrLimao";
+            this.pctrLimao.Size = new System.Drawing.Size(178, 211);
+            this.pctrLimao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrLimao.TabIndex = 5;
+            this.pctrLimao.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.radioButton1.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton1.Location = new System.Drawing.Point(120, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(128, 19);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Frutas Vermelhas";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.radioButton2.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton2.Location = new System.Drawing.Point(535, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 19);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Frutas Verdes";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.radioButton3.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton3.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton3.Location = new System.Drawing.Point(280, 160);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 19);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Morango";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.radioButton4.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton4.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton4.Location = new System.Drawing.Point(280, 373);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(90, 19);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Framboesa";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.radioButton5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Crimson;
+            this.radioButton5.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton5.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton5.Location = new System.Drawing.Point(280, 586);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(69, 19);
+            this.radioButton5.TabIndex = 10;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Tomate";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.radioButton6.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton6.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton6.Location = new System.Drawing.Point(681, 160);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(95, 19);
+            this.radioButton6.TabIndex = 11;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Maçã Verde";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.radioButton7.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton7.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton7.Location = new System.Drawing.Point(681, 373);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(49, 19);
+            this.radioButton7.TabIndex = 12;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Kiwi";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LimeGreen;
+            this.radioButton8.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.radioButton8.ForeColor = System.Drawing.SystemColors.Window;
+            this.radioButton8.Location = new System.Drawing.Point(681, 586);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(60, 19);
+            this.radioButton8.TabIndex = 13;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Limão";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.FlatAppearance.BorderSize = 0;
+            this.btnLimpar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnLimpar.Location = new System.Drawing.Point(328, 734);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(107, 42);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("SansSerif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button2.Location = new System.Drawing.Point(700, 734);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 42);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "SAIR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(894, 792);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pctrLimao);
+            this.Controls.Add(this.pctrKiwi);
+            this.Controls.Add(this.pctrMacaVerde);
+            this.Controls.Add(this.pctrTomate);
+            this.Controls.Add(this.pctrFramboesa);
+            this.Controls.Add(this.pctrMorango);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctrFramboesa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrMorango)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrTomate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrMacaVerde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrKiwi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrLimao)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctrFramboesa;
+        private System.Windows.Forms.PictureBox pctrMorango;
+        private System.Windows.Forms.PictureBox pctrTomate;
+        private System.Windows.Forms.PictureBox pctrMacaVerde;
+        private System.Windows.Forms.PictureBox pctrKiwi;
+        private System.Windows.Forms.PictureBox pctrLimao;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
